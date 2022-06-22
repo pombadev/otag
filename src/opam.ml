99622c4c5dc0868bde43cc886dbe3ed99dcd7eb7
pjmp <11157420+pjmp@users.noreply.github.com>
2022-06-22 19:37:03 +0545

    refactor: assert shape's fields has value

--- src/opam.ml
+++ src/opam.ml
@@ -12,33 +12,41 @@ end = struct
   let file =
     let file = OpamParser.FullPos.string [%blob "otag.opam"] "otag.opam" in
 
-    List.fold_right
-      (fun item init ->
-        match item.pelem with
-        | Variable (name, value) -> (
-            match name.pelem with
-            | "version" ->
-                let version = OpamPrinter.FullPos.value value in
-                let version =
-                  StringExtra.replace_all version ~pattern:"\"" ~with_:""
-                in
-                { init with version }
-            | "synopsis" ->
-                let synopsis = OpamPrinter.FullPos.value value in
-                let synopsis =
-                  StringExtra.replace_all synopsis ~pattern:"\"" ~with_:""
-                in
-                { init with synopsis }
-            | "description" ->
-                let desc = OpamPrinter.FullPos.value value in
-                let desc =
-                  StringExtra.replace_all desc ~pattern:"\"\"\"" ~with_:""
-                in
-                { init with description = desc }
-            | _ -> init)
-        | _ -> init)
-      file.file_contents
-      { version = ""; synopsis = ""; description = "" }
+    let value =
+      List.fold_right
+        (fun item init ->
+          match item.pelem with
+          | Variable (name, value) -> (
+              match name.pelem with
+              | "version" ->
+                  let version = OpamPrinter.FullPos.value value in
+                  let version =
+                    StringExtra.replace_all version ~pattern:"\"" ~with_:""
+                  in
+                  { init with version }
+              | "synopsis" ->
+                  let synopsis = OpamPrinter.FullPos.value value in
+                  let synopsis =
+                    StringExtra.replace_all synopsis ~pattern:"\"" ~with_:""
+                  in
+                  { init with synopsis }
+              | "description" ->
+                  let desc = OpamPrinter.FullPos.value value in
+                  let desc =
+                    StringExtra.replace_all desc ~pattern:"\"\"\"" ~with_:""
+                  in
+                  { init with description = desc }
+              | _ -> init)
+          | _ -> init)
+        file.file_contents
+        { version = ""; synopsis = ""; description = "" }
+    in
+
+    assert (String.length value.description > 0);
+    assert (String.length value.version > 0);
+    assert (String.length value.synopsis > 0);
+
+    value
 
   let version = file.version
   let description = file.description
